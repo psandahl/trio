feeec4c9fb639d2576df6ca848bf361b4d0bfc5e
Patrik Sandahl <patrik.sandahl@gmail.com>
2021-05-28 23:22:38 +0200

    Some refactoring

--- trio/common/matrix.py
+++ trio/common/matrix.py
@@ -1,8 +1,71 @@
+# Matrix helper functions.
+# The world coordinate system for trio is a right handed system like
+# the following.
+#
+#            Z
+#            |
+#            |____ Y
+#            /
+#           /
+#          X
+#
+# The Euler rotations - yaw, pitch and roll - will be a rotation on
+# the world axis in the order Y, Y and X.
+#
+#
+# The camera coordinate system is a right hand system where the axis
+# are permuted like:
+#
+#      X ____
+#           /|
+#          / |
+#         Z  Y
+#
+# I.e. the Z axis is the direction for the camera, X is to the right,
+# and Y is down.
+
+import math
 import numpy as np
 from scipy.linalg import svdvals
 
 
+def matrix_ypr(ypr):
+    """
+    Create a 3x3 rotation matrix relative the world coordinate system.
+    """
+    if type(ypr) == np.ndarray and ypr.size == 3:
+        sy = math.sin(ypr[0])
+        cy = math.cos(ypr[0])
+        sp = math.sin(ypr[1])
+        cp = math.cos(ypr[1])
+        sr = math.sin(ypr[2])
+        cr = math.cos(ypr[2])
+        m = [
+            cy * cp, cy * sp * sr - sy * cr, cy * sp * cr + sy * sr,
+            sy * cp, sy * sp * sr + cy * cr, sy * sp * cr - cy * sr,
+            -sp, cp * sr, cp * cr
+        ]
+        return np.array(m).reshape(3, 3)
+    else:
+        raise TypeError("ypr must be a numpy array of size three")
+
+
+def matrix_decompose_ypr(m):
+    """
+    Decompose a world rotation matrix into a tuple of yaw, pitch and roll.
+    """
+    if type(m) == np.ndarray and m.shape == (3, 3):
+        return (math.atan2(m[1, 0], m[0, 0]),
+                -math.asin(m[2, 0]),
+                math.atan2(m[2, 1], m[2, 2]))
+    else:
+        raise TypeError("m must be a 3x3 matrix")
+
+
 def matrix_rank(m):
+    """
+    Calculate the matrix rank for a matrix.
+    """
     svals = svdvals(m)
     rank = 0
     for s in svals:
--- trio/tests/common_matrix.py
+++ trio/tests/common_matrix.py
@@ -1,10 +1,59 @@
 import numpy as np
+import math
 import unittest
 
-from trio.common.matrix import matrix_rank
+from trio.common.matrix import matrix_rank, matrix_ypr, matrix_decompose_ypr
+
+
+def equal_matrices(m, n):
+    """
+    Helper function to test if two matrices are somewhat equal.
+    """
+    if m.shape == n.shape and m.dtype == m.dtype:
+        for row in range(m.shape[0]):
+            for col in range(m.shape[1]):
+                if math.fabs(m[row, col] - n[row, col]) > 0.000001:
+                    return False
+    else:
+        return False
+
+    return True
 
 
 class CommonMatrixTestCase(unittest.TestCase):
+    def test_matrix_ypr(self):
+        # Start with a matrix with no rotation.
+        zero_m = matrix_ypr(np.radians((0, 0, 0)))
+        self.assertEqual(3, matrix_rank(zero_m))
+        self.assertEqual((3, 3), zero_m.shape)
+
+        # It shall be equal to the identity matrix.
+        eye_m = np.eye(3, dtype=float)
+        self.assertTrue(equal_matrices(eye_m, zero_m))
+
+        # Continue with a "random" rotation.
+        random_m = matrix_ypr(np.radians((-87, 13.2, 37)))
+        self.assertEqual(3, matrix_rank(random_m))
+        self.assertEqual((3, 3), random_m.shape)
+
+        # Rotation matrix multiplied with its transpose shall be the identity.
+        self.assertTrue(equal_matrices(eye_m, random_m.T @ random_m))
+
+    def test_matrix_decompose_ypr(self):
+        # Start with a matrix with no rotation.
+        zero_m = matrix_ypr(np.radians((0, 0, 0)))
+        zero_d = np.degrees(matrix_decompose_ypr(zero_m))
+        self.assertAlmostEqual(0, zero_d[0])
+        self.assertAlmostEqual(0, zero_d[1])
+        self.assertAlmostEqual(0, zero_d[2])
+
+        # Continue with a "random" rotation.
+        random_m = matrix_ypr(np.radians((-87, 13.2, 37)))
+        random_d = np.degrees(matrix_decompose_ypr(random_m))
+        self.assertAlmostEqual(-87, random_d[0])
+        self.assertAlmostEqual(13.2, random_d[1])
+        self.assertAlmostEqual(37, random_d[2])
+
     def test_matrix_rank(self):
         # A matrix of zeros has rank zero.
         self.assertEqual(0, matrix_rank(np.zeros((3, 3))))
