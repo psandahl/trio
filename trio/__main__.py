95542f150da34071d76d74b9d68c31f465cb6339
Patrik Sandahl <patrik.sandahl@gmail.com>
2021-06-02 20:11:17 +0200

    Tracking buffer

--- trio/__main__.py
+++ trio/__main__.py
@@ -1,6 +1,12 @@
 import numpy as np
 import cv2 as cv
 
+from .image.tracking_buffer import TrackingBuffer
+
+
+def uv_to_int(uv):
+    return (int(round(uv[0])), int(round(uv[1])))
+
 
 def run_app():
     cap = cv.VideoCapture(cv.samples.findFile("vtest.avi"))
@@ -8,15 +14,32 @@ def run_app():
         print("Failed to open video")
         exit()
 
-    cv.namedWindow("Player")
+    tb = TrackingBuffer()
+
+    uv = (384.0, 288.0)
+    uv2 = uv
+
+    cv.namedWindow("Oldest")
+    cv.namedWindow("Newest")
     while True:
         ret, frame = cap.read()
         if not ret:
             print("Can't receive frame. Bye")
             break
 
-        cv.imshow("Player", frame)
-        key = cv.waitKey(30)
+        tb.add_image(frame)
+
+        uv2 = tb.track(uv2)
+
+        oldest_image = np.array(tb.oldest_image())
+        newest_image = np.array(tb.newest_image())
+
+        cv.drawMarker(oldest_image, uv_to_int(uv), (255, 0, 0))
+        cv.drawMarker(newest_image, uv_to_int(uv2), (0, 255, 0))
+
+        cv.imshow("Oldest", oldest_image)
+        cv.imshow("Newest", newest_image)
+        key = cv.waitKey(10)
         if key == 27 or key == ord('q'):
             break
 
---
+++ trio/image/tracking_buffer.py
@@ -0,0 +1,81 @@
+import numpy as np
+import cv2 as cv
+import math
+
+
+class TrackingBuffer:
+
+    orig_images = []
+    gray_images = []
+    flow_images = []
+    width = 10  # Must be at least 2
+
+    def __init__(self):
+        return
+
+    def add_image(self, image):
+        # Clean oldest data.
+        if len(self.orig_images) == self.width:
+            self.orig_images.pop(0)
+            self.gray_images.pop(0)
+            self.flow_images.pop(0)
+
+        # Gray convert the image.
+        gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+
+        self.orig_images.append(image)
+        self.gray_images.append(gray)
+
+        gs = len(self.gray_images)
+        if gs >= 2:
+            flow = cv.calcOpticalFlowFarneback(self.gray_images[gs - 2],
+                                               self.gray_images[gs - 1],
+                                               None, 0.5, 3, 15, 3, 5, 1.2, 0)
+            self.flow_images.append(flow)
+
+    def has_content(self):
+        return len(self.orig_images) > 0
+
+    def oldest_image(self):
+        return self.orig_images[0]
+
+    def newest_image(self):
+        return self.orig_images[len(self.orig_images) - 1]
+
+    def track(self, px):
+        if len(self.flow_images) == 0:
+            return px
+
+        uv = np.array(px)
+
+        #print("Start tracking - input u=%.5f, v=%.5f" % (uv[0], uv[1]))
+        for flow in self.flow_images:
+            # Integer and fractional parts of uv.
+            iu = math.floor(uv[0])
+            iv = math.floor(uv[1])
+
+            if iu < flow.shape[1] and iv < flow.shape[0]:
+                wu = uv[0] - iu
+                wv = uv[1] - iv
+
+                # Get interpolation weights.
+                w00 = (1.0 - wu) * (1.0 - wv)
+                w10 = (1.0 - wu) * wv
+                w01 = wu * (1.0 - wv)
+                w11 = wu * wv
+
+                # print("Interp. weights - w00=%.3f, w10=%.3f, w01=%.3f, w11=%.3f" %
+                #      (w00, w10, w01, w11))
+
+                weighted_change = flow[iv, iu] * w00 + flow[iv + 1, iu] * w10 \
+                    + flow[iv, iu + 1] * w01 + flow[iv + 1, iu + 1] * w11
+
+                # print("Weighted change - u=%.5f, v=%.5f" %
+                #      (weighted_change[0], weighted_change[1]))
+
+                uv += weighted_change
+                #print("Iteration update - u=%.5f, v=%.5f" % (uv[0], uv[1]))
+
+        #print("Final update - u=%.5f, v=%.5f" % (uv[0], uv[1]))
+
+        return (uv[0], uv[1])
