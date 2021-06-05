cf03199f9d8d8b973cfb8476e0e143e41aec5b3e
Patrik Sandahl <patrik.sandahl@gmail.com>
2021-06-05 23:49:36 +0200

    solve DLT

--- trio/common/linear.py
+++ trio/common/linear.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 from scipy import linalg
 
 from .camera import Camera
@@ -37,3 +38,48 @@ def triangulate(p0, uv0, p1, uv1):
         return euclidean(Vt[3:4, :].T)
     else:
         raise TypeError("Not the expected types for triangulate")
+
+
+def solve_dlt(points):
+    if type(points) == list and len(points) >= 6:
+        A = np.zeros((len(points) * 2, 12), dtype=float)
+
+        for i in range(len(points)):
+            point = points[i]
+
+            r1 = i * 2
+            A[r1, 0] = point["x"]
+            A[r1, 1] = point["y"]
+            A[r1, 2] = point["z"]
+            A[r1, 3] = 1.0
+            A[r1, 8] = -point["u"] * point["x"]
+            A[r1, 9] = -point["u"] * point["y"]
+            A[r1, 10] = -point["u"] * point["z"]
+            A[r1, 11] = -point["u"]
+
+            r2 = r1 + 1
+            A[r2, 4] = point["x"]
+            A[r2, 5] = point["y"]
+            A[r2, 6] = point["z"]
+            A[r2, 7] = 1.0
+            A[r2, 8] = -point["v"] * point["x"]
+            A[r2, 9] = -point["v"] * point["y"]
+            A[r2, 10] = -point["v"] * point["z"]
+            A[r2, 11] = -point["v"]
+
+        U, s, Vt = linalg.svd(A)
+        smallest_singular_value = s[s.shape[0] - 1]
+        if smallest_singular_value > 0.000001:
+            return (False, np.zeros((3, 4)))
+
+        h = Vt[11:, :]
+        if h[0, 11] < 0.0:
+            h *= -1.0
+
+        norm = math.sqrt(h[0, 8] ** 2 + h[0, 9] ** 2 + h[0, 10] ** 2)
+        h /= norm
+
+        p = h.reshape(3, 4)
+        return (True, p)
+    else:
+        raise TypeError("Not the expected types or length for solve_dlt")
--- trio/tests/common_linear.py
+++ trio/tests/common_linear.py
@@ -2,7 +2,9 @@ import numpy as np
 import unittest
 
 from trio.common.camera import Camera
-from trio.common.linear import triangulate
+from trio.common.linear import triangulate, solve_dlt
+from trio.common.math import euclidean, homogeneous
+from trio.common.matrix import matrix_look_at, matrix_decompose_ypr
 
 from .utils import equal_arrays
 
@@ -21,3 +23,44 @@ class CommonLinearTestCase(unittest.TestCase):
 
         self.assertTrue(equal_arrays(xyz, triangulate(
             c0.projection_matrix, uv0, c1.projection_matrix, uv1)))
+
+    def test_linear_simple_dlt(self):
+        # Setup camera.
+        ypr = matrix_decompose_ypr(
+            matrix_look_at(np.array((10, 10, 10)),
+                           np.array((0, 0, 0)),
+                           np.array((0, 0, 1))))
+
+        c = Camera(np.array((10, 10, 10)), np.array(ypr), np.radians((50, 45)))
+
+        # Produce points.
+        xyzs = [np.array((-3.2, 1.3, 1.1)),
+                np.array((-1.6, -2, 0.8)),
+                np.array((0, 0, -1)),
+                np.array((1.8, -1.6, -0.1)),
+                np.array((1.2, 2.1, -0.6)),
+                np.array((3.1, -2.7, 1.5)),
+                np.array((3.3, 2.7, 1.8))]
+
+        points = list()
+        for xyz in xyzs:
+            px = c.project(xyz)
+            point = dict()
+            point["x"] = xyz[0]
+            point["y"] = xyz[1]
+            point["z"] = xyz[2]
+            point["u"] = px[0]
+            point["v"] = px[1]
+            points.append(point)
+
+        # Run dlt.
+        res, p = solve_dlt(points)
+
+        self.assertTrue(res)
+
+        # Compare projections.
+        for xyz in xyzs:
+            px1 = c.project(xyz)
+            px2 = euclidean(p @ homogeneous(xyz))
+
+            self.assertTrue(equal_arrays(px1, px2))
